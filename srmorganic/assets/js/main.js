8207d2d67a2d0a167cba0d9dbcd62d6a1c915c44
Stephen R <54053384+htmldevmate@users.noreply.github.com>
2020-04-23 10:45:34 +0530

    initial commit

---
+++ srmorganic/assets/js/main.js
@@ -0,0 +1,17 @@
+
+// Background image
+jQuery('.imgLiquid').each(function() {
+    jQuery(this).find('img').hide();
+    var imgURL = jQuery(this).find('img').attr('src');
+    jQuery(this).css('background-image', 'url(' + imgURL + ')');
+});
+// Banner slider
+jQuery('.banner-slider').slick({
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+});
